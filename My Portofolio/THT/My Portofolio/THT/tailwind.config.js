f4161ce13b8e341c9a3a5661946aa67db96a5c0d
dandung21 <dandungsadat2004@gmail.com>
2025-11-08 13:50:10 +0700

    Add files via upload

---
+++ My Portofolio/THT/My Portofolio/THT/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./portofolio-tailwind.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
